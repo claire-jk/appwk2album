a9eb6f8e7831ee29778e5656921bb08176e0f0fc
claire-jk <15.29.15.29e@gmail.com>
2026-01-03 15:27:24 +0800

    platform.os

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,6 +6,8 @@ import { useFonts } from 'expo-font';
 import { Dimensions, Text, View } from 'react-native';
 // 1. 引入導航相關套件
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Platform } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import AlbumList from './Albumlist';
 import Header from './Header'; // 1. 引入 Header 組件
 const { width: windowWidth } = Dimensions.get('window');
@@ -14,6 +16,7 @@ const { width: windowWidth } = Dimensions.get('window');
 const Stack = createNativeStackNavigator();
 
 export default function HomeScreen() {
+  const insets = useSafeAreaInsets();
   let [fontsLoaded] = useFonts({
     'Zen': ZenKurenaido_400Regular,
     'Vibes': GreatVibes_400Regular,
@@ -36,7 +39,7 @@ export default function HomeScreen() {
         {/* 4. 註冊 AlbumList 頁面 */}
         <Stack.Screen name="AlbumHome">
           {(props) => (
-            <View style={{ flex: 1 }}>
+            <View style={{ flex: 1 , paddingTop: Platform.OS === 'android' ? 0 : insets.top}}>
               <Header titleFont="Caveat" />
               <AlbumList {...props} />
             </View>
